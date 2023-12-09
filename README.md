92fc2309fc149e9fceff49b1e0eedab25f2be40f
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-12-09 14:47:57 -0300

    inicio das offers

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# API G2A
+# API Gamivo
 
 ### Para rodar:
 Realizar o clone do repositório  
 Abrir o terminal no diretório do projeto  
 Rodar o comando "npm install"  
 Rodar o comando npm start  
+
+#### Documentação API Gamivo
+Exportar - https://www.gamivo.com/api-documentation/public  
+Importar - https://www.gamivo.com/assets/documents/documentation/API/public-api-import.pdf 
--- index.js
+++ index.js
@@ -18,25 +18,77 @@ app.listen(port, () => {
       console.log(`Listening to port ${port}.`);
 })
 
-app.get('/accountdata', async (req, res) => { //nosso endpoint
+// Accounts
+app.get('/account-data', async (req, res) => { //nosso endpoint
+      // Retorna o saldo e o fator de preço
       try {
-            
             // Faça a consulta à outra API usando Axios ou a biblioteca de sua escolha
-            // const response = await axios.get(`${url}/api/public/v1/accounts/data`, {headers});
-
             const response = await axios.get(`${url}/api/public/v1/accounts/data`, { // url + endpoint + token GAMIVO
                   headers: {
-                    'Authorization': `Bearer ${token}`
-                  //   'Content-Type': 'application/json',  // Opcional: ajuste conforme necessário
+                        'Authorization': `Bearer ${token}`
                   },
-                });
-        
+            });
             // Retorne os dados da consulta
             res.json(response.data); // nossa resposta
-          } catch (error) {
+      } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Erro ao consultar a API externa.' });
-          }
-      
-      // res.send('Server online');
+      }
 })
+
+app.get('/calculate-final-price', async (req, res) => {
+      // Retorna o preço final usando o fator de preço
+      const price = 10;
+      try {
+            const response = await axios.get(`${url}/api/public/v1/accounts/calculate-final-price/${price}`, {
+                  headers: {
+                        'Authorization': `Bearer ${token}`
+                  },
+            });
+            res.json(response.data); 
+      } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar a API externa.' });
+      }
+})
+
+// Offers
+app.get('/offer-list', async (req, res) => {
+      // Lista os jogos que estão a venda
+      const offset = 100; // A partir de qual jogo vai mostrar
+      const limit = 100; // Limit por página, acho que não pode ser maior que 100
+      try {
+            const response = await axios.get(`${url}/api/public/v1/offers?offset=${offset}&limit=${limit}`, {
+                  headers: {
+                        'Authorization': `Bearer ${token}`
+                        // 'Content-Type': 'application/json',
+                  },
+            });
+            const quantidade = response.data.length;
+            console.log(quantidade);
+            res.json(response.data);
+      } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao consultar a API externa.' });
+      }
+})
+
+// app.get('/offer-list', async (req, res) => {
+//       // Lista os jogos que estão a venda
+//       const offset = 100;
+//       const limit = 100;
+//       try {
+//             const response = await axios.get(`${url}/api/public/v1/offers?offset=${offset}&limit=${limit}`, {
+//                   headers: {
+//                         'Authorization': `Bearer ${token}`,
+//                         'Content-Type': 'application/json',
+//                   },
+//             });
+//             const quantidade = response.data.length;
+//             console.log(quantidade);
+//             res.json(response.data);
+//       } catch (error) {
+//             console.error(error);
+//             res.status(500).json({ error: 'Erro ao consultar a API externa.' });
+//       }
+// })
