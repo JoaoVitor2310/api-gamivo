adcc69e2b4a3949a7839b130941ecc21d789b8a7
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-12-16 16:28:09 -0300

    accounts routes

--- index.js
+++ index.js
@@ -23,35 +23,35 @@ app.listen(port, () => {
 })
 
 // Accounts
-app.get('/account-data', async (req, res) => { //nosso endpoint
-      // Retorna o saldo e o fator de preço
-      try {
-            // Faça a consulta à outra API usando Axios ou a biblioteca de sua escolha
-            const response = await axios.get(`${url}/api/public/v1/accounts/data`, { // url + endpoint + token GAMIVO
-                  headers: {
-                        'Authorization': `Bearer ${token}`
-                  },
-            });
-            // Retorne os dados da consulta
-            res.json(response.data); // nossa resposta
-      } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Erro ao consultar a API externa.' });
-      }
-})
+// app.get('/account-data', async (req, res) => { //nosso endpoint
+//       // Retorna o saldo e o fator de preço
+//       try {
+//             // Faça a consulta à outra API usando Axios ou a biblioteca de sua escolha
+//             const response = await axios.get(`${url}/api/public/v1/accounts/data`, { // url + endpoint + token GAMIVO
+//                   headers: {
+//                         'Authorization': `Bearer ${token}`
+//                   },
+//             });
+//             // Retorne os dados da consulta
+//             res.json(response.data); // nossa resposta
+//       } catch (error) {
+//             console.error(error);
+//             res.status(500).json({ error: 'Erro ao consultar a API externa.' });
+//       }
+// })
 
-app.get('/calculate-final-price', async (req, res) => {
-      // Retorna o preço final usando o fator de preço
-      const price = 10;
-      try {
-            const response = await axios.get(`${url}/api/public/v1/accounts/calculate-final-price/${price}`, {
-                  headers: {
-                        'Authorization': `Bearer ${token}`
-                  },
-            });
-            res.json(response.data);
-      } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Erro ao consultar a API externa.' });
-      }
-})
+// app.get('/calculate-final-price', async (req, res) => {
+//       // Retorna o preço final usando o fator de preço
+//       const price = 10;
+//       try {
+//             const response = await axios.get(`${url}/api/public/v1/accounts/calculate-final-price/${price}`, {
+//                   headers: {
+//                         'Authorization': `Bearer ${token}`
+//                   },
+//             });
+//             res.json(response.data);
+//       } catch (error) {
+//             console.error(error);
+//             res.status(500).json({ error: 'Erro ao consultar a API externa.' });
+//       }
+// })
--- routes/Router.js
+++ routes/Router.js
@@ -3,6 +3,7 @@ const router = express();
 
 router.use('/api/offers', require('./OffersRoutes'));
 router.use('/api/products', require('./ProductsRoutes'));
+router.use('/api/accounts', require('./AccountsRoutes'));
 // router.use('/api/job', require('./jobRoutes'));
 
 module.exports = router;
